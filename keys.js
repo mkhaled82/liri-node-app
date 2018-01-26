646284de44dc454f2fedbc3509241f8229f2a37e
Miriam Khaled <mkhaled82@gmail.com>
2018-01-26 16:18:00 -0700

    added keys.js file

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: '8umNIGMKTXdqxDUlim12ynBj8',
+  consumer_secret: 'EpPQVTBDK4aKuZjrkt4Fa4UAQhdCHitqyx5jBhsYZZ7dgy4QTP',
+  access_token_key: '3623257754-ajIC7ZOojibzLdMtcKqhr6e3sQ2i9voMwrpYcGW',
+  access_token_secret: 'A0otU82KA2Fskg8zUktcwJLokVr6O7UNVExYldYz0ejqR',
+};
+
+exports.spotify = {
+  id: '53d2e40c09fb4007b433b95e118e239e',
+  secret: '7098e5fc359e4d93a6ddd0d9c58086cb',
+ };
